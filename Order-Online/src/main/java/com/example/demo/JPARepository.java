57254cff5a69de3f73cc4b47706f428e71a6d070
iamakshaydatir18 <62378059+iamakshaydatir18@users.noreply.github.com>
2023-01-18 18:22:14 +0530

    Delete Order-Online directory

--- Order-Online/src/main/java/com/example/demo/JPARepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPARepository extends JpaRepository<fruit, Integer> {
-
-	  fruit findByName(String name);
-}
